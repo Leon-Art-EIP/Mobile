f2bf36490690aa2a8ffd871dd16aeff98a2fb2c4
Vivant-Garrigues <72013869+Vivant-Garrigues@users.noreply.github.com>
2023-06-24 16:42:58 +0200

    [ADD] basic elements

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Text } from 'react-native'
+import { Text, StyleSheet, View, TextInput, TouchableOpacity } from 'react-native'
 import { get } from '../constants/fetch';
 
 const Login = () => {
@@ -8,14 +8,138 @@ const Login = () => {
       console.log('Response: ', response);
     });
   }
+  const handleLogin = () => {
+    // Logique de connexion ici
+  };
 
+  const handleGoogleLogin = () => {
+    // Logique de connexion avec Google ici
+  };
+
+  const handleRegister = () => {
+    // Logique d'inscription ici
+  };
 
   useEffect(onPageRendered, []);
 
 
   return (
-    <Text>Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>LeonArt</Text>
+
+      <Text style={styles.loginTitle}>Login</Text>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your password"
+          secureTextEntry
+        />
+      </View>
+
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.orText}>Or</Text>
+
+      <TouchableOpacity style={styles.googleButton} onPress={handleGoogleLogin}>
+        <Text style={styles.googleButtonText}>Login with Google</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
+        <Text style={styles.registerButtonText}>Register</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 32,
+  },
+  loginTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  inputContainer: {
+    width: '100%',
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    padding: 8,
+    fontSize: 16,
+  },
+  loginButton: {
+    backgroundColor: 'blue',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 4,
+    marginBottom: 16,
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  orText: {
+    fontSize: 16,
+    marginBottom: 16,
+  },
+  googleButton: {
+    backgroundColor: 'red',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 4,
+    marginBottom: 16,
+  },
+  googleButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  registerButton: {
+    backgroundColor: 'gray',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 4,
+  },
+  registerButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default Login;
