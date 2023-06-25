37962c268d87f37be248967d31aaf0a621ab88cb
Vivant-Garrigues <72013869+Vivant-Garrigues@users.noreply.github.com>
2023-06-25 13:07:42 +0200

    [UPD] matching graphic chart

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, StyleSheet, View, TextInput, TouchableOpacity } from 'react-native'
 import { get } from '../constants/fetch';
 import Button, { ButtonProps } from '../components/Button';
 import Input, { InputProps } from '../components/Input';
 import Title, { TitleProps } from '../components/Title';
+import colors from "../constants/colors";
 
 const Login = () => {
   const handleLogin = () => {
@@ -26,38 +27,64 @@ const Login = () => {
     // Logique pour gérer le changement de mot de passe
   };
 
+  const handleForgotPassword = () => {
+    // Logique pour gérer le clic sur "Mot de passe oublié ?"
+  };
+
+  const handleRememberMeChange = (checked: boolean) => {
+    // Logique pour gérer le changement de la case "Se souvenir de moi"
+    setRememberMe(checked);
+  };
+
+  const [rememberMe, setRememberMe] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Title style={styles.title}>LeonArt</Title>
-
-      <Title style={styles.loginTitle}>Login</Title>
+      <View style={styles.titleView}>
+        <Title style={{color: colors.primary, fontSize: 70 }}>Leon</Title>
+        <Title style={{fontSize: 70}}>'Art</Title>
+      </View>
+      <Title style={styles.loginTitle}>Log In</Title>
 
       <Input
-        placeholder="Enter your email"
+        placeholder="Email"
         onTextChanged={handleEmailChange}
-        style={styles.input}
+        style={[styles.input, {marginHorizontal: 10}]}
       />
 
       <Input
-        placeholder="Enter your password"
+        placeholder="Mot de passe"
         onTextChanged={handlePasswordChange}
-        style={styles.input}
+        style={[styles.input, {marginHorizontal: 10}]}
         // secureTextEntry
       />
+
+      <View style={styles.rememberForgotContainer}>
+        <View style={styles.rememberMeContainer}>
+          {/* <CheckBox
+            value={rememberMe}
+            onValueChange={handleRememberMeChange}
+          /> */}
+          <Text style={styles.rememberMeText}>Se souvenir de moi</Text>
+          <TouchableOpacity onPress={handleForgotPassword}>
+            <Text style={styles.forgotPassword}>Mot de passe oublié ?</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
 
       <Button
         onPress={handleLogin}
         value="Login"
-        style={styles.loginButton}
+        style={[styles.loginButton, { backgroundColor: colors.primary }]}
         textStyle={styles.loginButtonText}
       />
 
-      <Text style={styles.orText}>Or</Text>
+      <Text style={styles.orText}>sOr</Text>
 
       <Button
         onPress={handleGoogleLogin}
         value="Login with Google"
-        style={styles.googleButton}
+        style={[styles.googleButton, { backgroundColor: colors.tertiary }]}
         textStyle={styles.googleButtonText}
       />
 
@@ -74,23 +101,21 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 32,
-  },
   loginTitle: {
-    fontSize: 18,
+    fontSize: 30,
     fontWeight: 'bold',
     marginBottom: 16,
+    marginTop: 20,
+    marginRight: 'auto',
+    marginLeft: 15,
     textAlign: 'center',
   },
   input: {
     marginBottom: 16,
+    width: '100%',
   },
   loginButton: {
     marginVertical: 16,
@@ -99,6 +124,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     textAlign: 'center',
+    color: 'white',
   },
   orText: {
     fontSize: 16,
@@ -122,6 +148,32 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     color: 'white',
+  },
+  titleView: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 70,
+    marginBottom: 40,
+  },
+  forgotPassword: {
+    fontSize: 14,
+    textDecorationLine: 'underline',
+    color: 'black',
+    marginLeft: 'auto'
+  },
+  rememberForgotContainer: {
+    flexDirection: 'row',
+    marginBottom: 16,
+    marginTop: 10,
+  },
+  rememberMeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  rememberMeText: {
+    marginLeft: 8,
   },
 });
 
