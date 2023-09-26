1b00a3b670b59945bae4913d8a28397ca2a59386
EvanKoe <evan.koehler@epitech.eu>
2023-09-26 14:21:07 +0200

    [ADD] presentation for FU

--- components/inbox/CommandsComponent.tsx
+++ components/inbox/CommandsComponent.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import colors from '../../constants/colors';
+import { flexRow, fwBold, ml8, mt4, mt8 } from '../../constants/styles';
 
 const CommandsComponent = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={{ color: colors.black }}>Command screen</Text>
+      <View style={flexRow}>
+        <Image style={{ width: 50, height: 50 }} source={require('../../assets/images/user.png')} />
+        <View style={[ml8, mt4]}>
+          <Text style={fwBold}>Mer de nuages</Text>
+          <Text>40€</Text>
+        </View>
+      </View>
     </ScrollView>
   );
 }
