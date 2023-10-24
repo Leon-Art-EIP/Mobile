4be8cf06339ef62394f278159535d231e3beeeed
mweber42 <marine.weber@epitech.eu>
2023-10-24 19:01:17 +0900

    [ADD] Addition Page Frontend

--- screens/AddPublication.tsx
+++ screens/AddPublication.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, Image } from 'react-native';
+import { TextInput, View, StyleSheet, Text, Image } from 'react-native';
 // import Icon from 'react-native-vector-icons/MaterialIcons';
 import { post } from '../constants/fetch';
 import colors from '../constants/colors';
@@ -8,11 +8,55 @@ import Button from '../components/Button';
 import TagButton from '../components/TagButton';
 import Toggle from '../assets/images/toggle.svg';
 
+import ImagePicker, { ImagePickerResponse } from 'react-native-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import env from '../env';
+
 const nextPage = () => {
 };
 
 const selectTag = () => {
 };
+// const YourComponent = () => {
+//   const [image, setImage] = useState<ImagePickerResponse | null>(null);
+
+//   const handleChoosePhoto = () => {
+//     const options = {
+//       noData: true,
+//     };
+//     ImagePicker.launchCamera(options, response => {
+//       if (response.uri) {
+//         setImage(response);
+//         uploadImage(response);
+//       }
+//     });
+//   };
+// }
+
+// const uploadImage = async image => {
+//   const data = new FormData();
+//   data.append('image', {
+//     uri: image.uri,
+//     type: 'image/jpeg',
+//     name: 'image.jpg',
+//   });
+
+//   try {
+//     const response = await axios.post('YOUR_API_ENDPOINT', data, {
+//       headers: {
+//         Accept: 'application/json',
+//         'Content-Type': 'multipart/form-data',
+//       },
+//     });
+//     console.log('Image uploaded successfully', response.data);
+//     // Add any handling of the API response here
+//   } catch (error) {
+//     console.error('Error uploading image', error);
+//     // Handle the error here
+//   }
+// };
+
 
 const SingleArt = () => {
   return (
@@ -24,19 +68,46 @@ const SingleArt = () => {
       <View style={{ flexDirection: 'row', paddingRight: 20, paddingLeft: 20 }}>
         <Text style={styles.artTitle}>Add Publication Page</Text>
       </View>
+      {/* <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {image && (
+        <Image
+          source={{ uri: image.uri }}
+          style={{ width: 200, height: 200, marginBottom: 10 }}
+        />
+      )}
+      <Button title="Take a Photo" onPress={handleChoosePhoto} />
+      </View> */}
       <View>
-        <Image style={styles.img} />
+      <TextInput
+          placeholder="Titre"
+          // onChangeText={handleEmailChange}
+          style={styles.textInput}
+          />
+        <TextInput
+          placeholder="Description"
+          // onChangeText={handleEmailChange}
+          style={styles.textInput}
+          />
+        <TextInput
+          placeholder="Prix"
+          // onChangeText={handleEmailChange}
+          style={styles.textInput}
+          />          
+        <TextInput
+        placeholder="Genre"
+        // onChangeText={handleEmailChange}
+        style={styles.textInput}
+        />
       </View>
-
       <View style={{ marginTop: 20 }}>
         <Button
-          value=""
+          value="Ajouter"
           onPress={nextPage}
         />
         <Button
           style={{ backgroundColor: colors.secondary }}
           textStyle={{ color: colors.black }}
-          value=""
+          value="Annuler"
           onPress={nextPage}
         />
       </View>
@@ -91,7 +162,14 @@ const styles = StyleSheet.create({
     vector: {
         width: 25,
         height: 31,
-    }
+    },
+    textInput: {
+      marginLeft: 15,
+      marginRight: 15,
+      backgroundColor: colors.secondary,
+      borderRadius: 7,
+      marginBottom: 20,
+    },
 });
 
 
